72900b4f458088c338277272dffcceeff8677b6f
Mukhtaram <mukhtaramsulaimonov@gmail.com>
2024-12-12 10:31:09 +0100

    Initial Commit

---
+++ BearB/BearBApp.swift
@@ -0,0 +1,17 @@
+//
+//  BearBApp.swift
+//  BearB
+//
+//  Created by Mukhtaram Sulaimonov on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BearBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
